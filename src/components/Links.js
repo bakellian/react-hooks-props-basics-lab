6d23213143553d0f99cd8ac56b52eee747514f74
Bridget Akellian <akellianbv@gmail.com>
2022-04-22 12:07:40 -0400

    links

---
+++ src/components/Links.js
@@ -0,0 +1,15 @@
+import React from "react";
+import user from "../data/user";
+
+function Links() {
+    return(
+        <div>
+            <h3>Links</h3>
+            <a href={ user.links.github }>Github</a>
+            <a href={ user.links.linkedin }>linkedin</a>
+        </div>
+       
+    )
+}
+
+export default Links;
